af27878b04611106f8deaf405bbd732b3a05a7e5
Tomek <kad.tom@wp.pl>
2025-05-04 12:09:55 +0200

    Inicjalizaca projektu

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Moje Portfolio</h1>
+        <p>Strona w budowie 🚧</p>
+      </header>
+      <main>
+        <p>Dziękuję za odwiedziny! Wkrótce pojawią się tutaj moje projekty, pliki i informacje kontaktowe.</p>
+      </main>
+      <footer>
+        <p>&copy; 2025 Tomasz Kadłubowski</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
